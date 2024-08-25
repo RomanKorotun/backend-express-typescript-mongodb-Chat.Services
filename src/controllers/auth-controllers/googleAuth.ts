260ee4cd7b8fc5916ed4f225d4a7571f3d25aba0
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-08-26 00:18:02 +0300

    fix

--- src/controllers/auth-controllers/googleAuth.ts
+++ src/controllers/auth-controllers/googleAuth.ts
@@ -23,7 +23,7 @@ const googleAuth = async (req: ICustomeRequest, res: Response) => {
   });
   await User.findByIdAndUpdate(id, { token });
   return res.redirect(
-    `https://romankorotun.github.io/test-task-frontend-Reenbit?token=${token}`
+    `https://romankorotun.github.io/test-task-frontend-Reenbit/register?token=${token}`
   );
 };
 export default googleAuth;
